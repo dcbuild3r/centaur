d53e98f7532c721ad29457ed0f906c2a5a46847d
dcbuild3r <dcbuilder@pm.me>
2026-08-07 16:24:57 +0200

    fix: restore dev console exposure

--- contrib/scripts/helm-chart-check.sh
+++ contrib/scripts/helm-chart-check.sh
@@ -24,3 +24,13 @@ helm template "$release_name" "$chart_dir" -n "$namespace" \
 
 grep -Fq "postgres-role-migration" "$aws_render"
 grep -Fq "REASSIGN OWNED BY %I TO %I" "$aws_render"
+
+# The AWS dev host shares one ingress between Console and Slackbot. Keep the
+# Console route ahead of Slackbot's catch-all and retain the SSO wiring.
+console_path_line="$(grep -nE '^[[:space:]]+- path: /console$' "$aws_render" | head -1 | cut -d: -f1)"
+catch_all_line="$(grep -nE '^[[:space:]]+- path: /$' "$aws_render" | head -1 | cut -d: -f1)"
+test -n "$console_path_line"
+test -n "$catch_all_line"
+test "$console_path_line" -lt "$catch_all_line"
+grep -Fq "CENTAUR_CONSOLE_BOOTSTRAP_ADMINS" "$aws_render"
+grep -Fq "IRON_CONTROL_GOOGLE_CLIENT_ID" "$aws_render"
